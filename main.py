2c85b2f7ca9397cb6c2a1231f799971367616860
what-da-fork <mason@wyd.mozmail.com>
2025-10-15 19:16:18 -0700

    up to ch1 lesson 5

---
+++ main.py
@@ -0,0 +1,32 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+client = genai.Client(api_key=api_key)
+
+if len(sys.argv) <= 1:
+    print("please provide a prompt")
+    sys.exit(1)
+if len(sys.argv) > 1:
+    prompt = sys.argv[1]  
+
+messages = [
+    types.Content(role="user", parts=[types.Part(text=prompt)]),
+]
+
+response = client.models.generate_content(
+    model='gemini-2.0-flash-001', contents=messages
+)
+
+def main():
+    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+    print("Response tokens:", response.usage_metadata.candidates_token_count)
+    print(response.text)
+
+if __name__ == "__main__":
+    main()
